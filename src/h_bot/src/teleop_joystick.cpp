1ffd5714316a0d3d93c06fe55938ae378266b8a6
zihao.he <awayhosi@hotmail.com>
2019-07-05 13:09:02 +0200

    Teleoperation through joystick now available.

--- src/h_bot/src/teleop_joystick.cpp
+++ src/h_bot/src/teleop_joystick.cpp
@@ -15,25 +15,26 @@ geometry_msgs::Twist vel;
 void callBack(const sensor_msgs::Joy::ConstPtr& joy) {
 	// here we finish the tasks of reading velocity from joystick and 
 	// publishing it to robot after rescaling it.
-	vel.linear.x = x_limit * joy->axes[0];
-	vel.angular.z = theta_limit * joy->axes[1];
+	vel.linear.x = x_limit * joy->axes[1];
+	vel.angular.z = theta_limit * joy->axes[0];
 }
 
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "teleop_joystick");
 	ros::NodeHandle nh;
-	ros::Rate loop_rate(10);
+	//ros::Rate loop_rate(10);
 	// this node publishes msgs of type: geometry_msgs/Twist
 	ros::Publisher pub;
 	// this node subscribes msgs of type: sensor_msgs/Joy
 	ros::Subscriber sub;
 	while(ros::ok()) {
-		pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+		pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1, true);
 		// Need to figure out: what does this 1 mean?
-		sub = nh.subscribe("joy",100,callBack);
+		sub = nh.subscribe("joy",1,callBack);
 		pub.publish(vel);
+		cout<<"Publishing..."<<endl;
 		ros::spinOnce();
-		loop_rate.sleep();
+		//loop_rate.sleep();
 	}
 	return 0;
 }
